bacde9e6e4fd502cdfccf8bfc24d48b53bbaea17
Timur Gafarov <gecko0307@gmail.com>
2026-09-24 18:11:43 +0300

    Update

--- tests/str/src/main.d
+++ tests/str/src/main.d
@@ -31,7 +31,8 @@ int main()
     
     TestClass t = New!TestClass();
     
-    String s4 = format("Name: {0}, age: {1}, employed: {2}, weight: {3}, class: {4}, ptr: {5}", "John Doe", 30, true, 80.0f, t, cast(void*)t);
+    int[5] arr = [0, 1, 2, 3, 4];
+    String s4 = format("Name: {0}, age: {1}, employed: {2}, weight: {3}, class: {4}, ptr: {5}, arr: {6}", "John Doe", 30, true, 80.0f, t, cast(void*)t, arr);
     printf("%s\n", s4.ptr);
     printf("%d\n", s4.length);
     s4.free();
